bd25c4eb3624d42ed8dd3450a256a0886daf1b3a
TheHudat <jhudakjh@gmail.com>
2026-02-01 09:30:35 -0500

    added leafnode class and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,8 +19,19 @@ class HTMLNode:
 
     def __repr__(self):
         prop_string = self.props_to_html()
-        child_string = ''
-        if self.children:
-            for child in self.children:
-                child_string += f'HTMLNode(TAG={child.tag}, VALUE="{child.value}", PROPS=({child.props_to_html()}) '
-        return f'HTMLNode(TAG={self.tag}, VALUE="{self.value}", CHILDREN=({child_string}), PROPS=({prop_string}))'
+        return f'HTMLNode(TAG={self.tag}, VALUE="{self.value}", CHILDREN={self.children}, PROPS=({prop_string}))'
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+        prop_string = self.props_to_html()
+        return f'LeafNode(TAG={self.tag}, VALUE="{self.value}", PROPS=({prop_string}))'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     child_node1 = HTMLNode('a', 'child_node1')
@@ -15,20 +15,45 @@ class TestHTMLNode(unittest.TestCase):
         solution = ' href="https://www.google.com" prop2="prop2 stuff"'
         self.assertEqual(self.test_node.props_to_html(), solution)
     
-    def test_print(self):
+    def test_htmlnode_print(self):
         self.maxDiff = None
         solution = (
         'HTMLNode(TAG=h1, VALUE="test value text", '
-        'CHILDREN=(HTMLNode(TAG=a, VALUE="child_node1", PROPS=() '
-        'HTMLNode(TAG=b, VALUE="child_node2", PROPS=() ), '
+        'CHILDREN=[HTMLNode(TAG=a, VALUE="child_node1", CHILDREN=None, PROPS=()), '
+        'HTMLNode(TAG=b, VALUE="child_node2", CHILDREN=None, PROPS=())], '
         'PROPS=( href="https://www.google.com" prop2="prop2 stuff"))'
         )
         self.assertEqual(self.test_node.__repr__(), solution)
     
-    def test_values(self):
+    def test_htmlnode_values(self):
         node = HTMLNode("div", "value text")
         self.assertEqual(node.tag,  "div")
         self.assertEqual(node.value, "value text")
         self.assertEqual(node.children, None)
         self.assertEqual(node.props, None)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_blockquote(self):
+        node = LeafNode("blockquote", "Hello, world!", {'href':'https://www.google.com'})
+        self.assertEqual(node.to_html(), '<blockquote href="https://www.google.com">Hello, world!</blockquote>')
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, 'text here')
+        self.assertEqual(node.to_html(), 'text here')
+
+    def test_leaf_print(self):
+        self.maxDiff = None
+        test_props = {
+        "href": "https://www.google.com",
+        "prop2": "prop2 stuff"
+        }
+        node = LeafNode("h1", "test value text", test_props)
+        solution = (
+        'LeafNode(TAG=h1, VALUE="test value text", '
+        'PROPS=( href="https://www.google.com" prop2="prop2 stuff"))'
+        )
+        self.assertEqual(node.__repr__(), solution)
         
