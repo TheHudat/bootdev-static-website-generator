02adce3ba48834c401e735aed030252f97c41595
TheHudat <jhudakjh@gmail.com>
2026-03-12 10:05:09 -0400

    added hosting from github

--- src/generate_page.py
+++ src/generate_page.py
@@ -3,21 +3,30 @@ import os
 from markdown_to_html import markdown_to_html_node
 from block_markdown import extract_title
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for item in os.listdir(dir_path_content):
         item_path = os.path.join(dir_path_content, item)
         if os.path.isfile(item_path) and os.path.splitext(item_path)[1] == ".md":
-            generate_page(item_path, template_path, os.path.join(dest_dir_path, "index.html"))
+            generate_page(
+                item_path, 
+                template_path, 
+                os.path.join(dest_dir_path, "index.html"),
+                basepath,
+                )
         elif os.path.isdir(item_path):
-            generate_page_recursive(item_path, template_path, os.path.join(dest_dir_path, item))
+            generate_page_recursive(
+                item_path, 
+                template_path, 
+                os.path.join(dest_dir_path, item),
+                basepath,
+                )
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     try:
         with open(from_path, 'r') as file:
             md_content = file.read()
-        print(f"File content {from_path} successfully saved to a variable...")
     except FileNotFoundError:
         print(f"Error: The file '{from_path}' was not found.")
     except Exception as e:
@@ -26,7 +35,6 @@ def generate_page(from_path, template_path, dest_path):
     try:
         with open(template_path, 'r') as file:
             template_content = file.read()
-        print(f"File content {template_path} successfully saved to a variable...")
     except FileNotFoundError:
         print(f"Error: The file '{template_path}' was not found.")
     except Exception as e:
@@ -38,6 +46,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template_content = template_content.replace("{{ Title }}", page_title)
     template_content = template_content.replace("{{ Content }}", html_file)
+    template_content = template_content.replace('href="/', f'href="/{basepath}')
+    template_content = template_content.replace('src="/', f'src="/{basepath}')
 
     dest_dirs = os.path.dirname(dest_path)
     if not os.path.exists(dest_dirs):
--- src/main.py
+++ src/main.py
@@ -1,28 +1,32 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_dir_content
 from generate_page import generate_page_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 dir_path_blog = dir_path_content + "/blog"
 template_path = "./template.html"
 
 def main():
+    basepath = os.path.dirname(sys.argv[1])
+
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
     
     print("Copying static files to public directory...")
-    copy_dir_content(dir_path_static, dir_path_public)
+    copy_dir_content(dir_path_static, dir_path_docs)
 
     print("Generating website...")
     generate_page_recursive(
         dir_path_content, 
         template_path, 
-        dir_path_public,
+        dir_path_docs,
+        basepath
     )
 
 main()
